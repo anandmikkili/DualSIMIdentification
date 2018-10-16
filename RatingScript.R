210c11cb0b10aecb82dfd14209cdd908796859cc
Rashmisathyananda <42572522+Rashmisathyananda@users.noreply.github.com>
2018-10-16 12:03:43 +0530

    Update RatingScript.R

--- RatingScript.R
+++ RatingScript.R
@@ -5,4 +5,5 @@ pred_file<-fread("{PredictionH}")
 pred_file[is.na(pred_file)] <- 0
 sum<-ifelse(pred_file$result1 > 0.5,1,0)+ifelse(pred_file$result2 > 0.5,1,0)+ifelse(pred_file$result3 > 0.5,1,0)+ifelse(pred_file$result4 > 0.5,1,0)+ifelse(pred_file$result5 > 0.5,1,0)+ifelse(pred_file$result6 > 0.5,1,0)
 pred_file$DualSimUsers = ifelse(sum >= 3,1,0)
+fwrite(pred_file,"{Rating}",quote=FALSE,na="")
 rm(list = ls(all = TRUE))
