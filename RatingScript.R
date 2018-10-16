9b82c8485b1f55533117fa5b313ebd72662eb6a9
Rashmisathyananda <42572522+Rashmisathyananda@users.noreply.github.com>
2018-10-16 13:26:23 +0530

    Update RatingScript.R

--- RatingScript.R
+++ RatingScript.R
@@ -1,7 +1,7 @@
 options(scipen=999999)
 options(java.parameters = "-Xmx5g")
 library(data.table)
-pred_file<-fread("{PredictionH}")
+pred_file<-fread("{MultiplePredictions}")
 pred_file[is.na(pred_file)] <- 0
 sum<-ifelse(pred_file$result1 > 0.5,1,0)+ifelse(pred_file$result2 > 0.5,1,0)+ifelse(pred_file$result3 > 0.5,1,0)+ifelse(pred_file$result4 > 0.5,1,0)+ifelse(pred_file$result5 > 0.5,1,0)+ifelse(pred_file$result6 > 0.5,1,0)
 pred_file$DualSimUsers = ifelse(sum >= 3,1,0)
