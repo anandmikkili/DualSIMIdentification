87b92ae377915690f80d26c7ca11147895d928d1
Rashmisathyananda <42572522+Rashmisathyananda@users.noreply.github.com>
2018-10-16 15:16:29 +0530

    Update CaseHandler.R

--- CaseHandler.R
+++ CaseHandler.R
@@ -7,7 +7,7 @@ dualsim1<-train[which((train$D_M1_REGION == "Gauteng")),]
 dualsim2<-train[which((train$D_M1_REGION == "KZN")),]
 f_path<-"{CaseHandler}"
 case_filename<-paste0(unlist(strsplit(f_path, ".", fixed=TRUE))[1],"_case1.txt")
-fwrite(dualsim1,case_filename,quote=FALSE,na="")
+fwrite(dualsim1$D_R_MSISDN,case_filename,quote=FALSE,na="")
 case_filename<-paste0(unlist(strsplit(f_path, ".", fixed=TRUE))[1],"_case2.txt")
-fwrite(dualsim2,case_filename,quote=FALSE,na="")
+fwrite(dualsim2$D_R_MSISDN,case_filename,quote=FALSE,na="")
 rm(list = ls(all = TRUE))
